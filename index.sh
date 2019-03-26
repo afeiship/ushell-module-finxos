56aa80ac411f74bc18c44fea2f84b97a59c21345
afeiship <1290657123@qq.com>
2019-03-26 08:50:01 +0800

    add: daily task

--- index.sh
+++ index.sh
@@ -2,6 +2,7 @@
 ## path:
 ROOT_PATH=$(dirname $BASH_SOURCE);
 
-alias ssh-finxos-test='ssh fzheng@118.25.123.163';
+alias ssh-finxos-test='ssh fzheng@212.64.40.40';
+alias daily='code ~/finxos/daily-works'
 
 unset ROOT_PATH;
